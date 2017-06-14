3e713bc163486987422c58aceb67c5f0fae050f5
smm15045281940 <smm15045281940@163.com>
2017-06-14 10:02:39 +0800

    优化预存款账户代码

--- app/src/main/java/com/gangjianwang/www/gangjianwang/AccountBalanceActivity.java
+++ app/src/main/java/com/gangjianwang/www/gangjianwang/AccountBalanceActivity.java
@@ -1,76 +1,75 @@
 package com.gangjianwang.www.gangjianwang;
 
 import android.graphics.Color;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import adapter.MyFragmentPagerAdapter;
 import fragment.AccountBalanceFragment;
 import fragment.AccountCashFragment;
 import fragment.RechargeDetailFragment;
 
 public class AccountBalanceActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private View rootView;
     private RelativeLayout mBackRl;
-    private TextView mAccountBalanceTitleTv;
-    private TextView mAccountBalanceTv, mRechargeDetailTv, mAccountCashTv;
-    private View mAccountBalanceV, mRechargeDetailV, mAccountCashV;
-    private FragmentManager mFragmentManager;
+    private TabLayout mTl;
+    private ViewPager mVp;
+    private String[] mTitle;
     private List<Fragment> mFragmentList;
-    private int curIndex;
+    private AccountBalanceFragment accountBalanceFragment;
+    private RechargeDetailFragment rechargeDetailFragment;
+    private AccountCashFragment accountCashFragment;
+    private MyFragmentPagerAdapter mFragmentPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_account_balance);
+        rootView = View.inflate(this, R.layout.activity_account_balance, null);
+        setContentView(rootView);
         initView();
-        initManager();
+        initData();
+        setData();
         setListener();
     }
 
     private void initView() {
-        mBackRl = (RelativeLayout) findViewById(R.id.rl_accountbalance_back);
-        mAccountBalanceTitleTv = (TextView) findViewById(R.id.tv_accountbalance_title);
-        mAccountBalanceTv = (TextView) findViewById(R.id.tv_accountbalance);
-        mRechargeDetailTv = (TextView) findViewById(R.id.tv_rechargedetail);
-        mAccountCashTv = (TextView) findViewById(R.id.tv_accountcash);
-        mAccountBalanceV = findViewById(R.id.v_accountbalance);
-        mRechargeDetailV = findViewById(R.id.v_rechargedetail);
-        mAccountCashV = findViewById(R.id.v_accountcash);
-        mAccountBalanceTv.setTextColor(Color.RED);
-        mAccountBalanceV.setVisibility(View.VISIBLE);
+        mBackRl = (RelativeLayout) rootView.findViewById(R.id.rl_accountbalance_back);
+        mTl = (TabLayout) rootView.findViewById(R.id.tl_accountbalance);
+        mVp = (ViewPager) rootView.findViewById(R.id.vp_accountbalance);
     }
 
-    private void initManager() {
-        mFragmentManager = getSupportFragmentManager();
+    private void initData() {
+        mTitle = getResources().getStringArray(R.array.accountbalance_title);
         mFragmentList = new ArrayList<>();
-        AccountBalanceFragment accountBalanceFragment = new AccountBalanceFragment();
-        RechargeDetailFragment rechargeDetailFragment = new RechargeDetailFragment();
-        AccountCashFragment accountCashFragment = new AccountCashFragment();
+        accountBalanceFragment = new AccountBalanceFragment();
+        rechargeDetailFragment = new RechargeDetailFragment();
+        accountCashFragment = new AccountCashFragment();
         mFragmentList.add(accountBalanceFragment);
         mFragmentList.add(rechargeDetailFragment);
         mFragmentList.add(accountCashFragment);
-        FragmentTransaction transaction = mFragmentManager.beginTransaction();
-        transaction.add(R.id.ll_accountbalance_sit, mFragmentList.get(0));
-        transaction.commit();
-        curIndex = 0;
+        mFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), mTitle, mFragmentList);
+    }
+
+    private void setData() {
+        mVp.setAdapter(mFragmentPagerAdapter);
+        mTl.setupWithViewPager(mVp);
+        mTl.setTabTextColors(Color.BLACK, Color.RED);
+        mTl.setSelectedTabIndicatorColor(Color.RED);
     }
 
     private void setListener() {
         mBackRl.setOnClickListener(this);
-        mAccountBalanceTv.setOnClickListener(this);
-        mRechargeDetailTv.setOnClickListener(this);
-        mAccountCashTv.setOnClickListener(this);
     }
 
     @Override
@@ -79,66 +78,8 @@ public class AccountBalanceActivity extends AppCompatActivity implements View.On
             case R.id.rl_accountbalance_back:
                 finish();
                 break;
-            case R.id.tv_accountbalance:
-                selected(0);
-                changeFragment(0);
+            default:
                 break;
-            case R.id.tv_rechargedetail:
-                selected(1);
-                changeFragment(1);
-                break;
-            case R.id.tv_accountcash:
-                selected(2);
-                changeFragment(2);
-                break;
-        }
-    }
-
-    private void selected(int a) {
-        switch (a) {
-            case 0:
-                mAccountBalanceTitleTv.setText("预存款账户");
-                mAccountBalanceTv.setTextColor(Color.RED);
-                mRechargeDetailTv.setTextColor(Color.BLACK);
-                mAccountCashTv.setTextColor(Color.BLACK);
-                mAccountBalanceV.setVisibility(View.VISIBLE);
-                mRechargeDetailV.setVisibility(View.INVISIBLE);
-                mAccountCashV.setVisibility(View.INVISIBLE);
-                break;
-            case 1:
-                mAccountBalanceTitleTv.setText("预存款充值");
-                mAccountBalanceTv.setTextColor(Color.BLACK);
-                mRechargeDetailTv.setTextColor(Color.RED);
-                mAccountCashTv.setTextColor(Color.BLACK);
-                mAccountBalanceV.setVisibility(View.INVISIBLE);
-                mRechargeDetailV.setVisibility(View.VISIBLE);
-                mAccountCashV.setVisibility(View.INVISIBLE);
-                break;
-            case 2:
-                mAccountBalanceTitleTv.setText("预存款提现");
-                mAccountBalanceTv.setTextColor(Color.BLACK);
-                mRechargeDetailTv.setTextColor(Color.BLACK);
-                mAccountCashTv.setTextColor(Color.RED);
-                mAccountBalanceV.setVisibility(View.INVISIBLE);
-                mRechargeDetailV.setVisibility(View.INVISIBLE);
-                mAccountCashV.setVisibility(View.VISIBLE);
-                break;
-        }
-    }
-
-    private void changeFragment(int b) {
-        if (curIndex != b) {
-            FragmentTransaction transaction = mFragmentManager.beginTransaction();
-            Fragment curFragment = mFragmentList.get(curIndex);
-            Fragment tarFragment = mFragmentList.get(b);
-            transaction.hide(curFragment);
-            if (tarFragment.isAdded()) {
-                transaction.show(tarFragment);
-            } else {
-                transaction.replace(R.id.ll_accountbalance_sit, tarFragment);
-            }
-            transaction.commit();
-            curIndex = b;
         }
     }
 }
