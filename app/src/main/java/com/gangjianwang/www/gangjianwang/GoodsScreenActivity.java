c583d652c4f3d146a1e3119c4ff4146b20f6e96a
smm15045281940 <smm15045281940@163.com>
2017-06-20 11:11:06 +0800

    优化商品筛选

--- app/src/main/java/com/gangjianwang/www/gangjianwang/GoodsScreenActivity.java
+++ app/src/main/java/com/gangjianwang/www/gangjianwang/GoodsScreenActivity.java
@@ -6,6 +6,8 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
@@ -31,13 +33,22 @@ import utils.ToastUtils;
 public class GoodsScreenActivity extends AppCompatActivity implements View.OnClickListener {
 
     private View rootView;
-    private RelativeLayout mBackRl, mResetRl;
+    private RelativeLayout mBackRl, mResetRl, mScreenRl;
+    private EditText mMinPriceEt, mMaxPriceEt;
     private Spinner mProvinceSp, mCitySp;
+    private CheckBox cbDonation, cbGroupBuy, cbLimitDiscount, cbVirtual;
+    private CheckBox cbPlatformSelfSupport;
+    private CheckBox cbSevenDayReturn, cbQualityPromise, cbDamageReplace, cbQuickFlow;
     private List<Province> mProvinceList = new ArrayList<>();
     private List<String> mCityList = new ArrayList<>();
     private AddressSpinnerAdapter mProvinceAdapter;
     private AddressCityAdapter mCityAdapter;
     private OkHttpClient okHttpClient;
+    private String priceSection;
+    private String goodsAddress;
+    private String goodsType;
+    private String storeType;
+    private String storeService;
 
     Handler handler = new Handler() {
         @Override
@@ -75,15 +86,26 @@ public class GoodsScreenActivity extends AppCompatActivity implements View.OnCli
     private void initView() {
         mBackRl = (RelativeLayout) rootView.findViewById(R.id.rl_goodsScreen_back);
         mResetRl = (RelativeLayout) rootView.findViewById(R.id.rl_goodsScreen_reset);
+        mScreenRl = (RelativeLayout) rootView.findViewById(R.id.rl_goodsScreen_screen);
+        mMinPriceEt = (EditText) rootView.findViewById(R.id.et_goodsScreen_minPrice);
+        mMaxPriceEt = (EditText) rootView.findViewById(R.id.et_goodsScreen_maxPrice);
         mProvinceSp = (Spinner) rootView.findViewById(R.id.sp_goodsScreen_province);
         mCitySp = (Spinner) rootView.findViewById(R.id.sp_goodsScreen_city);
+        cbDonation = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_donation);
+        cbGroupBuy = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_groupbuy);
+        cbLimitDiscount = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_limitdiscount);
+        cbVirtual = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_virtual);
+        cbPlatformSelfSupport = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_platformselfsupport);
+        cbSevenDayReturn = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_sevendayreturn);
+        cbQualityPromise = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_qualitypromise);
+        cbDamageReplace = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_damagereplace);
+        cbQuickFlow = (CheckBox) rootView.findViewById(R.id.cb_goods_screen_quickflow);
     }
 
     private void initData() {
         okHttpClient = new OkHttpClient();
         mProvinceAdapter = new AddressSpinnerAdapter(this, mProvinceList);
-//        mCityAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mCityList);
-        //TODO
+        mCityAdapter = new AddressCityAdapter(this, mCityList);
     }
 
     private void setData() {
@@ -94,6 +116,7 @@ public class GoodsScreenActivity extends AppCompatActivity implements View.OnCli
     private void setListener() {
         mBackRl.setOnClickListener(this);
         mResetRl.setOnClickListener(this);
+        mScreenRl.setOnClickListener(this);
     }
 
     private void loadData() {
@@ -139,6 +162,61 @@ public class GoodsScreenActivity extends AppCompatActivity implements View.OnCli
         return false;
     }
 
+    private void screen() {
+        priceSection = "价格区间：" + mMinPriceEt.getText().toString() + "-" + mMaxPriceEt.getText().toString();
+        goodsAddress = "商品所在地：" + ((Province) mProvinceSp.getSelectedItem()).getName() + " " + mCitySp.getSelectedItem().toString();
+        StringBuilder sbGoodsType = new StringBuilder();
+        if (cbDonation.isChecked()) {
+            sbGoodsType.append(" 赠品");
+        }
+        if (cbGroupBuy.isChecked()) {
+            sbGoodsType.append(" 团购");
+        }
+        if (cbLimitDiscount.isChecked()) {
+            sbGoodsType.append(" 限时折扣");
+        }
+        if (cbVirtual.isChecked()) {
+            sbGoodsType.append(" 虚拟");
+        }
+        goodsType = "商品类型：" + sbGoodsType.toString();
+        StringBuilder sbStoreType = new StringBuilder();
+        if (cbPlatformSelfSupport.isChecked()) {
+            sbStoreType.append(" 平台自营");
+        }
+        storeType = "店铺类型：" + sbStoreType.toString();
+        StringBuilder sbStoreService = new StringBuilder();
+        if (cbSevenDayReturn.isChecked()) {
+            sbStoreService.append(" 7天退货");
+        }
+        if (cbQualityPromise.isChecked()) {
+            sbStoreService.append(" 品质承诺");
+        }
+        if (cbDamageReplace.isChecked()) {
+            sbStoreService.append(" 破损补寄");
+        }
+        if (cbQuickFlow.isChecked()) {
+            sbStoreService.append(" 急速物流");
+        }
+        storeService = "店铺服务：" + sbStoreService.toString();
+        ToastUtils.toast(this, priceSection + "\n" + goodsAddress + "\n" + goodsType + "\n" + storeType + "\n" + storeService);
+    }
+
+    private void reset() {
+        mMinPriceEt.setText("");
+        mMaxPriceEt.setText("");
+        mProvinceSp.setSelection(0);
+        mCitySp.setSelection(0);
+        cbDonation.setChecked(false);
+        cbGroupBuy.setChecked(false);
+        cbLimitDiscount.setChecked(false);
+        cbVirtual.setChecked(false);
+        cbPlatformSelfSupport.setChecked(false);
+        cbSevenDayReturn.setChecked(false);
+        cbQualityPromise.setChecked(false);
+        cbDamageReplace.setChecked(false);
+        cbQuickFlow.setChecked(false);
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -146,7 +224,10 @@ public class GoodsScreenActivity extends AppCompatActivity implements View.OnCli
                 finish();
                 break;
             case R.id.rl_goodsScreen_reset:
-                ToastUtils.toast(GoodsScreenActivity.this, "重置");
+                reset();
+                break;
+            case R.id.rl_goodsScreen_screen:
+                screen();
                 break;
             default:
                 break;
