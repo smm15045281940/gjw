5b3b1d5911ace2ba5ef02eb92514dec9c369386d
smm15045281940 <smm15045281940@163.com>
2017-06-22 09:24:58 +0800

    优化商家报价头布局

--- app/src/main/java/com/gangjianwang/www/gangjianwang/StoreOfferActivity.java
+++ app/src/main/java/com/gangjianwang/www/gangjianwang/StoreOfferActivity.java
@@ -3,13 +3,17 @@ package com.gangjianwang.www.gangjianwang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.RelativeLayout;
 
+import customview.MyRefreshListView;
+
 public class StoreOfferActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private View rootView;
+    private View rootView, emptyView;
     private RelativeLayout backRl;
+    private MyRefreshListView mLv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,12 @@ public class StoreOfferActivity extends AppCompatActivity implements View.OnClic
 
     private void initView() {
         backRl = (RelativeLayout) rootView.findViewById(R.id.rl_store_offer_back);
+        mLv = (MyRefreshListView) rootView.findViewById(R.id.lv_store_offer);
+        emptyView = View.inflate(this, R.layout.empty_store_offer, null);
+        emptyView.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        ((ViewGroup) mLv.getParent()).addView(emptyView);
+        emptyView.setVisibility(View.GONE);
+        mLv.setEmptyView(emptyView);
     }
 
     private void setListener() {
